85a8bb03ab06f12a2eeda05fd6604945463de941
user <user@192.168.219.100>
2023-01-16 23:57:44 +0900

    1/16 김은영은 보아라

--- frontend/public/JS/과제/과제4.js
+++ frontend/public/JS/과제/과제4.js
@@ -3,7 +3,7 @@ console.log('작동')
 
 
 
-	let newBook,list_index,b_list,가능여부='';
+	let newBook,list_index,가능여부='';
 	let 도서목록= [ '혼자공부하는자바' , '이것이자바다' , '열혈 C언어'  ]
 	let 대여목록= [ '혼자공부하는자바' , '이것이자바다' ]
 	
@@ -11,8 +11,11 @@ console.log('작동')
 
 
 function 중복함수(){//f s
+	let htm1=''
+	let b_list=''
 	
-	b_list='<tr><th>번호</th><th>도서</th><th>도서대여여부</th><th>버튼</th></tr>'
+	b_list='<tr><th>번호</th><th>도서명</th><th>도서대여여부</th><th>버튼</th></tr>'
+	html = `<tr><th>번호</th>	<th>도서명</th><th>도서대여여부</th><th colspan="2" >비고</th>	</tr>` 			
 	for(let i=0;i<도서목록.length;i++){// for s
 		b_list+=`<tr>
 					<td>${ i+1 }</td>
@@ -20,11 +23,30 @@ function 중복함수(){//f s
 					<td>${ 대여목록.includes( 도서목록[i] ) ? '불가능':'가능' }</td>
 					
 					<td><button onclick="삭제버튼(${i})">삭제</button></td></tr>`
+		
+		html += `<tr>
+					<td>${ i+1 }</td>
+					<td>${ 도서목록[i] }</td>
+					<td>${ 대여목록.includes( 도서목록[i] ) ? '대여중':'대여가능' }</td>
+					
+				<td><button onclick="대여(${i})">대여버튼</button></td>
+				<td><button onclick="반납(${i})">반납버튼</button></td>
+				</tr>`				
+					
 		}//for e
-	}//f e
+		
+		
+	document.querySelector('.b_list').innerHTML=b_list
+		
+	document.querySelector('.표').innerHTML = html	
+		
+}			
+										
+	
+	
 		
 	
-	function 등록(){
+	function 등록(){//f s
 		
 		newBook=document.querySelector('.newBook').value
 		list_index=도서목록.indexOf(newBook)
@@ -40,39 +62,42 @@ function 중복함수(){//f s
 			
 			
 			}// if e		
-		else{
+		else{//e s
 				도서목록.push(newBook)
 				/*console.log(도서목록)*/
-			}
+			}//e e
 		
 		중복함수()
-		출력()
-		
-		document.querySelector('.b_list').innerHTML=b_list
 	
-	}
+		
+		
+	
+	}// f e
 
 
-function 삭제버튼(i){
+function 삭제버튼(i){// f s
 	
 	if( 대여목록.indexOf( 도서목록[i] ) >= 0 ){
-		console.log('삭제가 불가능 합니다.')
+		alert('대여중이므로 삭제가 불가능합니다.')
 	}
 	else{
-	도서목록.splice(i,1)
-	console.log(도서목록)
+		도서목록.splice(i,1)
+		console.log(도서목록)
+		alert('삭제완료.')
+		
 	}
 	중복함수();
 		
 		
 
-	document.querySelector('.b_list').innerHTML=b_list
-}
 
+}// f e
+
+// 여기까지장민정
 
 /*******************************************************************************************/
 
-
+//여기부터 김은영
 
 
 
@@ -90,10 +115,11 @@ function 대여(i){
 	}else{
 		alert('대여가능합니다.')
 		대여목록.push( 도서목록[i] )
-		출력()
-		중복함수()
-		console.log('대여목록:'+대여목록)
+		
+		
 	}
+	중복함수()
+		console.log('대여목록:'+대여목록)
 	
 	
 	
@@ -117,13 +143,12 @@ function 반납(i){
 	if(  삭제번호 >=0   ){
 		alert('대여중 반납가능 ')
 		대여목록.splice( 삭제번호 , 1  )
-		출력()
 	}else{
 		alert('대여중이 아니라 반납불가능 ')
-		중복함수()	
+		
 	}
 	
-	
+	중복함수()	
 /*	for(let i=0; i<도서목록.length; i++){
 		for(let j=0; j<대여목록.length; j++){
 			if(도서목록[i]==대여목록[i]){alert(도서목록[i]+'는 반납했습니다.')
@@ -134,33 +159,6 @@ function 반납(i){
 }
 
 
-
-
-출력()
-function 출력() {
-	let htm1=''
-	
-	
-	let html = `<tr>			
-					<th>번호</th>
-					<th>도서명</th>
-					<th>도서대여여부</th>
-					<th colspan="2" >비고</th>
-				</tr>` 			
-	for( let i = 0 ; i<도서목록.length; i++ ){ // 5. 내용추가 [ 반복 이용한 배열내 요소 출력 ]
-		html += `<tr>
-					<td>${ i+1 }</td>
-					<td>${ 도서목록[i] }</td>
-					<td>${ 대여목록.includes( 도서목록[i] ) ? '대여중':'대여가능' }</td>
-					
-				<td><button onclick="대여(${i})">대여버튼</button></td>
-				<td><button onclick="반납(${i})">반납버튼</button></td>
-				</tr>`	
-								
-										
-	} 
-	document.querySelector('.표').innerHTML = html
-}
 
 
 
